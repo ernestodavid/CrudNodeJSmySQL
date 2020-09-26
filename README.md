20ae9e56f46680ba3a2f84d392eece3f53246007
Ernesto <david.montano.rosabal@gmail.com>
2020-09-26 22:38:20 +0200

    subiendo json

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CrudNodeJSmySQL
 interaccion con base de datos en nodejs
+
+Commnados ejecutados 
+
+npm install express mysql express-myconnection morgan ejs
---
+++ crud-nodejs-mysql/package.js
@@ -0,0 +1,12 @@
+{
+    "name" : "crud-nodejs-mysql",
+    "version": "1.0.0",
+    "description": "",
+    "main":"index.js",
+    "scripts": {
+        "test":"echo \"Errror: no test specified\" && exit 1"
+    },
+    "keywords":[],
+    "author":"Ernesto David Montano Rosabal",
+    "license":"ISC"
+}
